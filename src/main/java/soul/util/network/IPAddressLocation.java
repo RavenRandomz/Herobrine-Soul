f86022db01929653d441dddf91ef601c0b30d4cc
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-05-04 12:14:19 -0500

    Ignore swp

---
+++ src/main/java/soul/util/network/IPAddressLocation.java
@@ -0,0 +1,16 @@
+package soul.util.network;
+
+public class IPAddressLocation
+{
+	private String address;
+	private int port;
+
+	public IPAddressLocation(String IPAddress, int port)
+	{
+		address = IPAddress;
+		this.port = port;
+	}
+}
+
+
+
