2d2b376ca53f218a2ba6474287230ae663342cfb
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-05-04 13:05:43 -0500

    Add Command Handling to Client

--- src/main/java/soul/soulclient/SoulClient.java
+++ src/main/java/soul/soulclient/SoulClient.java
@@ -2,23 +2,39 @@ package soul.soulclient;
 import java.net.Socket;
 
 import soul.util.network.IPAddressLocation;
+import soul.soulclient.HerobrineCommandHandler;
 
 import java.net.UnknownHostException;
 import java.io.IOException;
 
-import java.util.Queue;
 public class SoulClient
 {
 	private HerobrineListener herobrineListener;
+	private HerobrineCommandHandler commandHandler;
 	/**
 	 * UnknownHost means failure to connect
 	 * IOException means failure to obtain stream
 	 */
+
+	public SoulClient()
+	{
+		commandHandler = new HerobrineCommandHandler();
+	}
 	public void connect(IPAddressLocation addressLocation) throws UnknownHostException, IOException
 	{
 		Socket herobrineSocket = new Socket(addressLocation.getAddress(), addressLocation.getPort());
 		herobrineListener = new HerobrineListener(herobrineSocket, this);
+
 		Thread herobrineListenerthread = new Thread(herobrineListener);
 		herobrineListenerthread.start();
+
+		Thread commandHandlerThread = new Thread(commandHandler);
+		commandHandlerThread.start();
+
+	}
+
+	public void addHerobrineCommand(String command)
+	{
+		commandHandler.addCommand(command);
 	}
 }
