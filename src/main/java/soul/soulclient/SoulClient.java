7403271a19fd2ec9be0b770b9d0305d6e7c211c4
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-05-04 12:34:23 -0500

    Add connection function to SoulClient

--- src/main/java/soul/soulclient/SoulClient.java
+++ src/main/java/soul/soulclient/SoulClient.java
@@ -3,11 +3,22 @@ import java.net.Socket;
 
 import soul.util.network.IPAddressLocation;
 
+import java.net.UnknownHostException;
+import java.io.IOException;
+
+import java.util.Queue;
 public class SoulClient
 {
-
-	public void connect()
+	private HerobrineListener herobrineListener;
+	/**
+	 * UnknownHost means failure to connect
+	 * IOException means failure to obtain stream
+	 */
+	public void connect(IPAddressLocation addressLocation) throws UnknownHostException, IOException
 	{
+		Socket herobrineSocket = new Socket(addressLocation.getAddress(), addressLocation.getPort());
+		herobrineListener = new HerobrineListener(herobrineSocket, this);
+		Thread herobrineListenerthread = new Thread(herobrineListener);
+		herobrineListenerthread.start();
 	}
-
 }
