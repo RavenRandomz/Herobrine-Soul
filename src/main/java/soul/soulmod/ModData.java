2421b6edb6e2337ccc91698f4a2b0d82987095cc
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-05-06 16:52:43 -0500

    Initialize routines in ModData

--- src/main/java/soul/soulmod/ModData.java
+++ src/main/java/soul/soulmod/ModData.java
@@ -2,11 +2,20 @@ package soul.soulmod;
 
 import soul.soulmod.kings.KingRegistry;
 import soul.soulmod.routines.RoutineRegistry;
+import soul.soulmod.routines.IRoutine;
 import soul.soulmod.routines.EchoRoutine;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 public class ModData
 {
 	private static KingRegistry kings = new KingRegistry();
-	private static RoutineRegistry routines = new RoutineRegistry();
+	private static ArrayList<IRoutine> rawRoutines = new ArrayList<IRoutine>(
+			Arrays.asList(
+				new EchoRoutine()
+				)
+			);
+	private static RoutineRegistry routines = new RoutineRegistry(rawRoutines);
 
 	public static KingRegistry getKings()
 	{
