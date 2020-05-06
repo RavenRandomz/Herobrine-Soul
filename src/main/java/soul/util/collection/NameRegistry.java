8623307607e2ab63c46ae5fccdbcdf8ed87d9031
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-05-06 18:19:12 -0500

    Add name registry

---
+++ src/main/java/soul/util/collection/NameRegistry.java
@@ -0,0 +1,32 @@
+package soul.util.collection;
+
+
+import soul.util.collection.Named;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+public class NameRegistry<E>
+{
+	private HashMap<String, Named> namedElementMap = new HashMap<>();
+
+	public NameRegistry()
+	{
+	}
+	public NameRegistry(ArrayList<Named> elements)
+	{
+		for(Named element : elements)
+		{
+			addElement(element);
+		}
+	}
+	public void addElement(Named element)
+	{
+		String name = element.getName();
+		namedElementMap.put(name, element);
+	}
+
+	public E getElement(String name)
+	{
+		return (E)namedElementMap.get(name);
+	}
+}
