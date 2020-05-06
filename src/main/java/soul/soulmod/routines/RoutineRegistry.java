e7b985cfd6b0454a1d81c7a970a4e30d677951e5
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-05-06 16:44:52 -0500

    Add constructor that accepts a list of routines

--- src/main/java/soul/soulmod/routines/RoutineRegistry.java
+++ src/main/java/soul/soulmod/routines/RoutineRegistry.java
@@ -3,10 +3,21 @@ package soul.soulmod.routines;
 import soul.soulmod.routines.IRoutine;
 
 import java.util.HashMap;
+import java.util.ArrayList;
 public class RoutineRegistry
 {
 	private HashMap<String, IRoutine> nameRoutineMap = new HashMap<>();
 
+	public RoutineRegistry()
+	{
+	}
+	public RoutineRegistry(ArrayList<IRoutine> routines)
+	{
+		for(IRoutine routine : routines)
+		{
+			addRoutine(routine);
+		}
+	}
 	public void addRoutine(IRoutine routine)
 	{
 		String name = routine.getName();
