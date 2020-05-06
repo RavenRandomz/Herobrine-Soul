73f5298b2494094d150a2ac5e3d8c248a3c04470
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-05-06 15:45:14 -0500

    add possible hail routine

---
+++ src/main/java/soul/soulmod/routines/HailRoutine.java
@@ -0,0 +1,49 @@
+package soul.soulmod.routines;
+
+import soul.util.wrapper.LocalPlayerWrapper;
+import soul.soulmod.SoulClientMod;
+import soul.soulmod.routines.IRoutine;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.ServerChatEvent;
+
+
+public class HailRoutine implements IRoutine
+{
+	private SoulClientMod soulClientMod;
+	public HailRoutine(SoulClientMod soulClientMod)
+	{
+		this.soulClientMod = soulClientMod;
+	}
+	@Override
+	public void activate()
+	{
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@Override
+	public void deactivate()
+	{
+		MinecraftForge.EVENT_BUS.unregister(this);
+	}
+
+	@Override
+	public String getName()
+	{
+		return "hail";
+	}
+
+	@SubscribeEvent
+	public void onChat(ServerChatEvent chatEvent)
+	{
+		String playerName = chatEvent.getUsername();
+		if(soulClientMod.isUnderKing(playerName))
+		{
+			LocalPlayerWrapper.sendChat(chatEvent.getMessage());
+		}
+
+	}
+
+
+}
