9a9d54f2ecd74d0e0b206ef103519a0367e815e3
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-05-06 16:38:02 -0500

    Add global routine registry

--- src/main/java/soul/soulmod/ModData.java
+++ src/main/java/soul/soulmod/ModData.java
@@ -1,12 +1,19 @@
 package soul.soulmod;
 
 import soul.soulmod.kings.KingRegistry;
+import soul.soulmod.routines.RoutineRegistry;
 public class ModData
 {
 	private static KingRegistry kings = new KingRegistry();
+	private static RoutineRegistry routines = new RoutineRegistry();
 
-	public static KingRegistry getKingRegistry()
+	public static KingRegistry getKings()
 	{
 		return kings;
+	}
+
+	public static RoutineRegistry getRoutines()
+	{
+		return routines;
 	}
 }
