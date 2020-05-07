3d94f4015c523fafe1c4e5e630717bc8036de8f5
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-05-06 19:55:45 -0500

    Add better command handler

--- src/main/java/soul/util/commands/CommandHandler.java
+++ src/main/java/soul/util/commands/CommandHandler.java
@@ -3,6 +3,10 @@ package soul.util.commands;
 import soul.util.collection.NameRegistry;
 
 import soul.util.commands.Command;
+
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Arrays;
 public class CommandHandler
 {
 	private NameRegistry<Command> commandRegistry = new NameRegistry<>();
@@ -25,8 +29,23 @@ public class CommandHandler
 		return commandRegistry.hasElement(commandInvocation);
 	}
 
+	public boolean commandIsValid(String rawCommand)
+	{
+		StringTokenizer commandTokens = new StringTokenizer(rawCommand);
+		return commandExists(commandTokens.nextToken());
+		
+	}
+
 	public void executeCommand(String name, String[] args)
 	{
+		commandRegistry.getElement(name).execute(args);
+	}
+
+	public void executeCommand(String rawCommand)
+	{
+		ArrayList<String> splitCommand = new ArrayList<String>(Arrays.asList(rawCommand.split(" ")));
+		String name = splitCommand.get(0);
+		String[] args = splitCommand.subList(1, splitCommand.size() -1).toArray(new String[0]);
 		commandRegistry.getElement(name).execute(args);
 	}
 }
