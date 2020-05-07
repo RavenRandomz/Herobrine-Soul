0b126f2d8f1a4d1f9b06b5cc0e1b21f26212f608
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-05-07 11:12:54 -0500

    Salvage Echo to Yee Yee

---
+++ src/main/java/soul/soulmod/routines/YeeYeeRoutine.java
@@ -0,0 +1,45 @@
+package soul.soulmod.routines;
+
+import soul.util.wrapper.LocalPlayerWrapper;
+import soul.soulmod.routines.IRoutine;
+import soul.soulmod.ModData;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.ServerChatEvent;
+
+
+public class YeeYeeRoutine implements IRoutine
+{
+	private static String Yee = "Yee Yee!"; //This string name is illegal. But fuck you, I'm a rebel. Liberate the namez!!!
+	@Override
+	public void activate()
+	{
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@Override
+	public void deactivate()
+	{
+		MinecraftForge.EVENT_BUS.unregister(this);
+	}
+
+	@Override
+	public String getName()
+	{
+		return "echo";
+	}
+
+	@SubscribeEvent
+	public void onChat(ServerChatEvent chatEvent)
+	{
+		String playerName = chatEvent.getUsername();
+		if(ModData.getKings().hasKing(playerName))
+		{
+			LocalPlayerWrapper.sendChat(Yee);
+		}
+
+	}
+
+
+}
