3a780d78a54d2ad5a0022f0f950c76eeafc41a11
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-05-07 14:54:44 -0500

    Add SpamRoutine

--- src/main/java/soul/soulmod/routines/SpamRoutine.java
+++ src/main/java/soul/soulmod/routines/SpamRoutine.java
@@ -8,47 +8,64 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.ServerChatEvent;
 
-
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 public class SpamRoutine implements IRoutine
 {
+	private static long DEFAULT_SPAM_INTERVAL_MS = 1000;
+	private String spamMessage;
+	private long spamIntervalMilliseconds;
+	private ScheduledExecutorService periodicExecutor = Executors.newScheduledThreadPool(1);
+	private ScheduledFuture<?> spammerHandler;
 	private class Spammer implements Runnable
 	{
+		private String spamMessage;
+		public Spammer(String message)
+		{
+			spamMessage = message;
+		}
 		@Override
 		public void run()
 		{
-			LocalPlayerWrapper.sendChat("Spam");
+			LocalPlayerWrapper.sendChat(spamMessage);
 
 		}
+	}
+
+	@Override
+	public void activate(String[] args)
+	{
+		ArrayList<String> argsList = new ArrayList<String>(Arrays.asList(args));
+		spamMessage = args[0];
+		if(argsList.size() == 2)
+		{
+			spamIntervalMilliseconds = Integer.parseInt(argsList.get(1));
+		}
+		else
+		{
+			spamIntervalMilliseconds = DEFAULT_SPAM_INTERVAL_MS;
+		}
+		Spammer theSpammer = new Spammer(spamMessage);
+		Thread spamThread = new Thread(theSpammer);
+		spammerHandler = periodicExecutor.schedule(spamThread, spamIntervalMilliseconds, TimeUnit.MILLISECONDS);
 	}
 	@Override
 	public void activate()
 	{
-		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	@Override
 	public void deactivate()
 	{
-		MinecraftForge.EVENT_BUS.unregister(this);
 	}
 
 	@Override
 	public String getName()
 	{
-		return "yee";
+		return "spam";
 	}
-
-	@SubscribeEvent
-	public void onChat(ServerChatEvent chatEvent)
-	{
-		String playerName = chatEvent.getUsername();
-		if(ModData.getKings().hasKing(playerName))
-		{
-			LocalPlayerWrapper.sendChat(Yee);
-		}
-
-	}
-
-
 }
