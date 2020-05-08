30fc6bc8840460814d22684ae4d4da95f018230c
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-05-07 20:57:17 -0500

    Have Yee Yee be cancellable

--- src/main/java/soul/soulmod/commands/YeeYeeCommand.java
+++ src/main/java/soul/soulmod/commands/YeeYeeCommand.java
@@ -2,11 +2,18 @@ package soul.soulmod.commands;
 
 import soul.soulmod.ModData;
 import soul.util.commands.Command;
+import soul.util.commands.Cancellable;
 
 import soul.util.wrapper.LocalPlayerWrapper;
 
-public class YeeYeeCommand implements Command
+public class YeeYeeCommand implements Command, Cancellable
 {
+	@Override
+	public void cancel()
+	{
+		ModData.getRoutines().getRoutine("yee").deactivate();
+	}
+
 	@Override
 	public String getInvocation()
 	{
