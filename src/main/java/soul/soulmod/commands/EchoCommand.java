775591f481db46bda090fab3a2a28bf52e20929b
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-05-07 20:54:59 -0500

    Have echo command cancellable

--- src/main/java/soul/soulmod/commands/EchoCommand.java
+++ src/main/java/soul/soulmod/commands/EchoCommand.java
@@ -2,10 +2,11 @@ package soul.soulmod.commands;
 
 import soul.soulmod.ModData;
 import soul.util.commands.Command;
+import soul.util.commands.Cancellable;
 
 import soul.util.wrapper.LocalPlayerWrapper;
 
-public class EchoCommand implements Command
+public class EchoCommand implements Command, Cancellable
 {
 	@Override
 	public String getInvocation()
@@ -19,6 +20,12 @@ public class EchoCommand implements Command
 		//LocalPlayerWrapper.sendChat("Attempting Echo");
 		//LocalPlayerWrapper.sendChat(new Boolean(ModData.getRoutines().hasRoutine("echo")).toString());
 		ModData.getRoutines().getRoutine("echo").activate();
+	}
+
+	@Override
+	public void cancel()
+	{
+		ModData.getRoutines().getRoutine("echo").deactivate();
 	}
 
 	@Override
