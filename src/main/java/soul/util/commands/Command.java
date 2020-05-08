00371d34664d1292d4c7bfef0a4c8c3d2f709ea2
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-05-07 21:10:56 -0500

    Change command interface to use raw arguments

--- src/main/java/soul/util/commands/Command.java
+++ src/main/java/soul/util/commands/Command.java
@@ -5,5 +5,5 @@ import soul.util.collection.Named;
 public interface Command extends Named
 {
 	public String getInvocation();
-	public void execute(String[] args);
+	public void execute(String rawArgs);
 }
