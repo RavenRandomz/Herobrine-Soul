b989b1c38b9555e882a016ef99755e0df93e4728
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-05-09 01:19:58 -0500

    Move routines to API

--- src/main/java/soul/soulmod/routines/IRoutine.java
+++
@@ -1,9 +0,0 @@
-package soul.soulmod.routines;
-
-public interface IRoutine
-{
-	public void activate();
-	public void activate(String[] args);
-	public void deactivate();
-	public String getName();
-}
